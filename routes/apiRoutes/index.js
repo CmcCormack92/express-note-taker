1a2265166957b084bdf8d103e53ad9d0891fe512
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2022-01-09 17:34:58 -0500

    file structure

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes');
+
+
+
+module.exports = router;
