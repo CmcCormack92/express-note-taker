cc69d4a01eb91fbb1eed9d617bb34a6bfda9ee65
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2022-01-09 18:45:44 -0500

    add routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
-const animalRoutes = require('../apiRoutes/animalRoutes');
-const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes');
+const { createNewNote, validateNotes } = require('../../lib/notes');
+const { notes } = require('../../Develop/db/db');
 
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
 
+router.post('/notes', (req, res) => {
+    if (!validateNotes(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+    }
+});
 
 module.exports = router;
